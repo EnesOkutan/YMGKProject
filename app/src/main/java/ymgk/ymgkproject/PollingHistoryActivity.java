863526d7e539ebcd933c55610c49b842d36a5cce
Enes Okutan <enes.okutan@hotmail.com>
2019-12-11 21:04:15 +0300

    Add activity polling history to polling history module

---
+++ app/src/main/java/ymgk/ymgkproject/PollingHistoryActivity.java
@@ -0,0 +1,13 @@
+package ymgk.ymgkproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PollingHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polling_history);
+    }
+}

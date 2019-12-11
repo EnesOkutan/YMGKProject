c48c85aa22a99bbfc5e1bd9f4428146d9c44e832
Enes Okutan <enes.okutan@hotmail.com>
2019-12-11 21:12:15 +0300

    Add activity active polling to take a pool

---
+++ app/src/main/java/ymgk/ymgkproject/ActivePollingActivity.java
@@ -0,0 +1,13 @@
+package ymgk.ymgkproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivePollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_polling);
+    }
+}
